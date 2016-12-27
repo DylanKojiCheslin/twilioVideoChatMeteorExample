9a5bcad25f3127225c34ceef715a4a5d5714e3f7
dylan <dylan.kojicheslin@gmail.com>
2016-12-27 02:34:58 -0700

    added random check example settings main requestVideoChat

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # twilioVideoChatMeteorExample
 twilio video chat in meteor
+
+create a twilio account
+
+copy example-settings.json to settings.json
+
+put your twilio account data into settings.json
+
+run with
+
+meteor --settings settings.json
---
+++ server/requestVideoChatAccess.js
@@ -0,0 +1,27 @@
+import { AccessToken } from 'twilio';
+const ConversationsGrant = AccessToken.ConversationsGrant;
+Meteor.methods({
+    'requestVideoChatAccess' : function(roomName, clientIdentity){
+        check(roomName, String);
+        check(roomName, String);
+        let token, response;
+        let grant = new ConversationsGrant();
+        token = new AccessToken(
+        Meteor.settings.private.twilio.accountSid,
+        Meteor.settings.private.twilio.apiKey,
+        Meteor.settings.private.twilio.apiKeySecret
+        );
+        //assign identity to token
+        token.identity = clientIdentity;
+
+        //grant access to Video
+        grant.configurationProfileSid = Meteor.settings.private.twilio.configurationProfileSid;
+        token.addGrant(grant);
+
+        response = {
+          identity: identity,
+          token: token.toJwt()
+        }
+        return response;
+    }
+});
